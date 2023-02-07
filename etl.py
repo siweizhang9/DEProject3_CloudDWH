bb8b0f5c1633e23542e3e6ea05a2f0fc9baa3dda
siweizhang9 <31421213+siweizhang9@users.noreply.github.com>
2023-02-07 00:08:23 -0500

    Add docstrings
    Add recommended docstrings per PEP 257 style guide

--- etl.py
+++ etl.py
@@ -1,21 +1,30 @@
+"""import configparser and psycopgs modules and self-defined SQL queires"""
 import configparser
 import psycopg2
 from sql_queries import *
 
 
 def load_staging_tables(cur, conn):
+    """load data from S3 to staging tables in redshift"""
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """transform data from staging tables into fact and dimensions tables"""
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    1. Load configuration parameters
+    2. Connect to Redshift Cluster with psycopg2
+    3. load staging tables
+    4. insert data into dimension and fact tables
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
@@ -29,7 +38,7 @@ def main():
 
     conn = psycopg2.connect(aws_db_paremeters)
     cur = conn.cursor()
-    
+
     load_staging_tables(cur, conn)
     insert_tables(cur, conn)
 
